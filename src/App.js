47b1fbcd5a07a601c0aa9e6c33ae4a6711752338
Davi Teodoro <davi.a.teodoro@gmail.com>
2021-11-11 12:08:27 -0300

    setup commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
+import React from 'react';
 import logo from './logo.svg';
+import tauriCircles from './tauri.svg';
+import tauriWord from './wordmark.svg';
 import './App.css';
 
+import {
+  app,
+  dialog,
+  event,
+  fs,
+  clipboard,
+  globalShortcut,
+} from '@tauri-apps/api';
+
+// With the Tauri API npm package:
+import { invoke } from '@tauri-apps/api/tauri';
+
+// With the Tauri global script, enabled when `tauri.conf.json > build > withGlobalTauri` is set to true:
+// const invoke = window.__TAURI__.invoke;
+
+// Invoke the command
+invoke('my_custom_command');
+
 function App() {
+  // console.log(clipboard.readText());
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="inline-logo">
+          <img src={tauriCircles} className="App-logo rotate" alt="logo" />
+          <img src={tauriWord} className="App-logo smaller" alt="logo" />
+        </div>
+        <a
+          className="App-link"
+          href="https://tauri.studio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Tauri
+        </a>
+        <img src={logo} className="App-logo rotate" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +48,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
       </header>
     </div>
   );
